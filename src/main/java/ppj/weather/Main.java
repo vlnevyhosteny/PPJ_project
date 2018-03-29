36dfa7569ec8d246b44349b4a85865a11a50d12f
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2018-03-29 11:04:09 +0200

    Basic logback configuration done.

--- src/main/java/ppj/weather/Main.java
+++ src/main/java/ppj/weather/Main.java
@@ -11,7 +11,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         SpringApplication app = new SpringApplication(Main.class);
-        app.setAdditionalProfiles("devel");
 
         ApplicationContext ctx = app.run(args);
 
