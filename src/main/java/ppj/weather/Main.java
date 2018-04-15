e10238fe346f9eb52678c6d46a3f335b94647583
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2018-04-15 21:17:28 +0200

    Changes dependecies to same as in samples.

--- src/main/java/ppj/weather/Main.java
+++ src/main/java/ppj/weather/Main.java
@@ -1,29 +1,28 @@
 package ppj.weather;
 
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Profile;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.persistence.EntityManagerFactory;
-import org.hibernate.SessionFactory;
 
 import ppj.weather.configs.AppConfiguration;
 import ppj.weather.model.CityDao;
 import ppj.weather.model.StateDao;
 import ppj.weather.provisioning.Provisioner;
 
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
 @SpringBootApplication
 @EnableTransactionManagement
 @EntityScan("ppj.weather.model")
-@ComponentScan(basePackages = {"ppj.weather"})
 public class Main {
 
     @Bean
@@ -55,8 +54,6 @@ public class Main {
         return new Provisioner();
     }
 
-    @Autowired
-    private static StateDao stateDao;
 
     public static void main(String[] args) throws Exception {
 
